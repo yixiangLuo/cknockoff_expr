d6a9e933aacb1f08e9cc2b91191d1d33a4e28f28
yixiangLuo <yixiangluo0111@gmail.com>
2021-12-17 17:29:04 -0800

    add all expr

--- jobs/all_exprs.sh
+++ jobs/all_exprs.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
-N_JOBS=7
+N_JOBS=800
 
-sh do_expr.sh test $N_JOBS
+sh do_expr.sh main_expr_largeScale $N_JOBS
+
+sh do_expr.sh lambdasmax_stat $N_JOBS
+
+sh do_expr.sh robust_noisyBeta $N_JOBS
+
+sh do_expr.sh robust_tNoise $N_JOBS
+
+sh do_expr.sh knockoff_stats $N_JOBS
