114eb33520d90dd919911605a7e84512d8de3416
yixiangLuo <yixiangluo0111@gmail.com>
2024-10-20 15:54:15 -0700

    Remove ignored files

--- jobs/expr_names.sh
+++ jobs/expr_names.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "power_gain"
-
-# echo "main_expr_10 main_expr_30 main_expr_50 multiple_kn lambdasmax_stat robust_noisyBeta robust_tNoise knockoff_stats"
+echo "main_expr_10 main_expr_30 power_gain multiple_kn lambdasmax_stat robust_noisyBeta robust_tNoise knockoff_stats"
 
  
